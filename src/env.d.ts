aec0121db7ed21f867b8e54f6230434f7f144cf6
Zakhar Gulchak <gulchak.zahar@gmail.com>
2023-09-07 23:58:01 +0200

    chore: interim changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,7 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
     readonly BUILDER_API_PUBLIC_KEY: string;
+    readonly BUILDER_PAGES_MODEL: string;
     readonly BUILDER_BLOGPOST_MODEL: string;
+    readonly BUILDER_NEWS_MODEL: string;
 }
